37a04d7eac25bb8a2e6632b1ceac349524f7851b
Zoutekaas <luukperdaems@Hotmail.com>
2022-11-22 15:56:22 +0100

    reverted main function changes, since this causes odd issues

--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,61 @@
--- setFullscreen is placed here for debuggin purposes
--- love.window.setFullscreen(true, 'desktop') 
+love.window.setFullscreen(true, 'desktop')
 
-local love_handler = require "love_handler"
+EXPECTED_SCREEN_HEIGHT = 1080
+EXPECTED_SCREEN_WIDTH = 1920
+
+ScreenWidhtScalingFactor = love.graphics.getWidth() / EXPECTED_SCREEN_WIDTH
+ScreenHeightScalingFactor = love.graphics.getHeight() / EXPECTED_SCREEN_HEIGHT
+
+local TICK_RATE = 1 / 100
+local MAX_FRAME_SKIP = 25
+
+local active_panel_manager = require "panel_manager.active_panel_manager"
 
 function love.run()
-    return love_handler.construct()
+    if love.load then love.load() end
+    if love.timer then love.timer.step() end
+
+    local lag = 0.0
+
+    return function()
+
+        if love.event then
+            love.event.pump()
+            for name, a, b, c, d, e, f in love.event.poll() do
+                if name == "quit" then
+                    if not love.quit or not love.quit() then
+                        return a or 0
+                    end
+                end
+                love.handlers[name](a, b, c, d, e, f)
+            end
+        end
+        if love.timer then
+            lag = math.min(lag + love.timer.step(), TICK_RATE * MAX_FRAME_SKIP)
+        end
+        while lag >= TICK_RATE do
+            if love.update then love.update(TICK_RATE) end
+            lag = lag - TICK_RATE
+        end
+        if love.graphics and love.graphics.isActive() then
+            love.graphics.origin()
+            love.graphics.clear(love.graphics.getBackgroundColor())
+
+            if love.draw then love.draw() end
+            love.graphics.present()
+        end
+        if love.timer then love.timer.sleep(1 / 60) end
+    end
+end
+
+function love.load()
+    active_panel_manager.load()
+end
+
+function love.update(dt)
+    active_panel_manager.update(dt)
+end
+
+function love.draw()
+    active_panel_manager.draw()
 end
